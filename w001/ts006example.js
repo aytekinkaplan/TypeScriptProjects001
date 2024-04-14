83466542eddc276dce330f4d456931b784896cdb
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-15 00:16:17 +0300

    New Commit

---
+++ w001/ts006example.js
@@ -0,0 +1,5 @@
+var person = {
+    name: "Ahmed",
+    age: 100
+};
+console.log(person["name"], person["age"]);
---
+++ w001/ts006example.ts
@@ -0,0 +1,6 @@
+let person: { name: string, age: number } = {
+    name: "Ahmed",
+    age: 100
+}
+
+console.log(person["name"], person["age"]);
